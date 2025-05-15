2690da30654da433a89846e045b29a284728d403
COLLINCE <omondicolli@gmail.com>
2025-05-15 14:01:27 +0300

    Modifying the script and html to add the date field and displaying data alongside the text and also the delete button and functionality

--- scripts/todo_list.js
+++ scripts/todo_list.js
@@ -2,10 +2,16 @@ const todoList = []; // Initalizing an empty array
 
 // Get the to do text from the input box and add it to the array
 function addTodo() {
-    const textelement = document.querySelector('.js_todo_text');
-    todo_text = textelement.value;
+    const textelement = document.querySelector('.js_todo_text'); // Fetching the todo text value
+    const todo_text = textelement.value;
 
-    todoList.push(todo_text)
+    const dateInputElement = document.querySelector('.js_todo_date'); // fetching the todo date
+    const due_date = dateInputElement.value;
+
+    todoList.push({ // Adding the fetched elements together into a list in object format
+        name : todo_text, 
+        dueDate:due_date
+    })
 
     textelement.value = '' // Resets back the input box an empty string thereby displaying the placeholder text
 
@@ -15,11 +21,24 @@ function addTodo() {
 function displayTodo() {
     let todoListHTML = '';
 
-    for (i=0; i<todoList.length; i++) {
-        const todo = todoList[i];
-        const html = `<p>${todo}</p>`;
+    for (let i=0; i<todoList.length; i++) {
+        const todoObj = todoList[i];
+        const name = todoObj.name; // fetching the todo from the object
+        const dueDate = todoObj.due_date; // fetching the date from the object
+        // const { name, dueDate } = todoObj;
+
+        // displaying the todo and duedate on the page and adding a delete functionality
+        const html = `
+            <p>
+                ${name} ${dueDate} 
+                <button onclick="
+                    todoList.splice(${i}, 1);
+                    displayTodo();
+                ">Delete</button>
+            </p>`;
         todoListHTML += html
 
-        document.querySelector('.js_todo_display').innerHTML = todoListHTML;
+        document.querySelector('.js_todo_display').innerHTML = todoListHTML; // rendering the fetched text and date on the paragraph template as innerHTML
     }
+    
 }
