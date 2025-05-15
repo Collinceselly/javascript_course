59d85e05c6791dec1eea7d4a93b461141be23c47
COLLINCE <omondicolli@gmail.com>
2025-05-15 15:46:16 +0300

    css to style the buttons and some classes on the ,js and .html files

--- scripts/todo_list.js
+++ scripts/todo_list.js
@@ -10,10 +10,12 @@ function addTodo() {
 
     todoList.push({ // Adding the fetched elements together into a list in object format
         name : todo_text, 
-        dueDate:due_date
-    })
+        dueDate : due_date
+    });
 
     textelement.value = '' // Resets back the input box an empty string thereby displaying the placeholder text
+
+    console.log(todoList)
 
     displayTodo()
 }
@@ -24,18 +26,19 @@ function displayTodo() {
     for (let i=0; i<todoList.length; i++) {
         const todoObj = todoList[i];
         const name = todoObj.name; // fetching the todo from the object
-        const dueDate = todoObj.due_date; // fetching the date from the object
+        const dueDate = todoObj.dueDate; // fetching the date from the object
         // const { name, dueDate } = todoObj;
 
         // displaying the todo and duedate on the page and adding a delete functionality
         const html = `
-            <p>
-                ${name} ${dueDate} 
-                <button onclick="
-                    todoList.splice(${i}, 1);
-                    displayTodo();
-                ">Delete</button>
-            </p>`;
+            <div>${name}</div>
+            <div>${dueDate}</div>
+                 
+            <button class="delete_button" onclick="
+                todoList.splice(${i}, 1);
+                displayTodo();
+            ">Delete</button>
+            `;
         todoListHTML += html
 
         document.querySelector('.js_todo_display').innerHTML = todoListHTML; // rendering the fetched text and date on the paragraph template as innerHTML
